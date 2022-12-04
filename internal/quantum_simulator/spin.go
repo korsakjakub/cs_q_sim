3bfa80e76dc32635b5f9546f685529a6e063b069
Jakub Korsak <jakub.korsak@protonmail.com>
2022-12-04 19:30:36 +0100

    1-body spin matrices and unit tests

---
+++ internal/quantum_simulator/spin.go
@@ -0,0 +1,65 @@
+package quantum_simulator
+
+import (
+	"math"
+
+	"gonum.org/v1/gonum/mat"
+)
+
+func delta(a, b float64) float64 {
+	if math.Abs(a-b) < 1e-4 {
+		return 1.0
+	}
+	return 0.0
+}
+
+func elSz(row, col float64) float64 {
+	return delta(row, col) * float64(col)
+}
+
+func elSp(row, col, spin float64) float64 {
+	return delta(row, col+1) * math.Sqrt(spin*(spin+1.0)-col*row)
+}
+
+func elSm(row, col, spin float64) float64 {
+	return delta(row+1, col) * math.Sqrt(spin*(spin+1.0)-col*row)
+}
+
+func Sm(spin float64) *mat.Dense {
+	dim := int(2.0*spin + 1.0)
+	data := make([]float64, dim*dim)
+	i := 0
+	for m := spin; m >= -spin; m -= 1.0 {
+		for n := spin; n >= -spin; n -= 1.0 {
+			data[i] = elSm(m, n, spin)
+			i += 1
+		}
+	}
+	return mat.NewDense(int(dim), int(dim), data)
+}
+
+func Sp(spin float64) *mat.Dense {
+	dim := int(2.0*spin + 1.0)
+	data := make([]float64, dim*dim)
+	i := 0
+	for m := spin; m >= -spin; m -= 1.0 {
+		for n := spin; n >= -spin; n -= 1.0 {
+			data[i] = elSp(m, n, spin)
+			i += 1
+		}
+	}
+	return mat.NewDense(int(dim), int(dim), data)
+}
+
+func Sz(spin float64) *mat.Dense {
+	dim := int(2.0*spin + 1.0)
+	data := make([]float64, dim*dim)
+	i := 0
+	for m := spin; m >= -spin; m -= 1.0 {
+		for n := spin; n >= -spin; n -= 1.0 {
+			data[i] = elSz(m, n)
+			i += 1
+		}
+	}
+	return mat.NewDense(int(dim), int(dim), data)
+}
---
+++ internal/quantum_simulator/spin_test.go
@@ -0,0 +1,102 @@
+package quantum_simulator
+
+import (
+	"math"
+	"reflect"
+	"testing"
+
+	"gonum.org/v1/gonum/mat"
+)
+
+func TestSz(t *testing.T) {
+	type args struct {
+		spin float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want *mat.Dense
+	}{
+		{
+			name: "spin-half pauli",
+			args: args{spin: 0.5},
+			want: mat.NewDense(2, 2, []float64{0.5, 0.0, 0.0, -0.5}),
+		},
+		{
+			name: "spin-1 pauli",
+			args: args{spin: 1.0},
+			want: mat.NewDense(3, 3, []float64{1.0, 0.0, 0.0,
+				0.0, 0.0, 0.0,
+				0.0, 0.0, -1.0}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Sz(tt.args.spin); !mat.EqualApprox(got, tt.want, 1e-4) { // reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Sz() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSp(t *testing.T) {
+	type args struct {
+		spin float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want *mat.Dense
+	}{
+		{
+			name: "spin-half pauli",
+			args: args{spin: 0.5},
+			want: mat.NewDense(2, 2, []float64{0.0, 1.0, 0.0, 0.0}),
+		},
+		{
+			name: "spin-1 pauli",
+			args: args{spin: 1.0},
+			want: mat.NewDense(3, 3, []float64{0.0, math.Sqrt2, 0.0,
+				0.0, 0.0, math.Sqrt2,
+				0.0, 0.0, 0.0}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Sp(tt.args.spin); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Sp() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSm(t *testing.T) {
+	type args struct {
+		spin float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want *mat.Dense
+	}{
+		{
+			name: "spin-half pauli",
+			args: args{spin: 0.5},
+			want: mat.NewDense(2, 2, []float64{0.0, 0.0, 1.0, 0.0}),
+		},
+		{
+			name: "spin-1 pauli",
+			args: args{spin: 1.0},
+			want: mat.NewDense(3, 3, []float64{0.0, 0.0, 0.0,
+				math.Sqrt2, 0.0, 0.0,
+				0.0, math.Sqrt2, 0.0}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Sm(tt.args.spin); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Sm() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
