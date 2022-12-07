05ac7cb9ed3b743c6cf05e34e1f44ef1c5a56336
Jakub Korsak <jakub.korsak@protonmail.com>
2022-12-07 11:25:02 +0100

    implement calculating hamiltonians

--- internal/quantum_simulator/system.go
+++ internal/quantum_simulator/system.go
@@ -3,42 +3,95 @@ package quantum_simulator
 import (
 	"math"
 	"strconv"
+
+	"gonum.org/v1/gonum/mat"
 )
 
 const e0 = 8.854e-12
 
 type System struct {
-	CentralSpin State
-	Bath        []State
+	CentralSpin   State
+	Bath          []State
+	PhysicsConfig PhysicsConfig
 }
 
 type State struct {
-	Angle            float64
-	Distance         float64
-	MagneticField    float64
-	InteractionForce float64
+	Angle    float64
+	Distance float64
 }
 
-func (s *System) forces(pc PhysicsConfig) []float64 {
+func (s *System) forceAt(j int) float64 {
+	if j == 0 {
+		return 0.0
+	}
 
-	bc, err := strconv.Atoi(pc.BathCount)
-
+	muMol, err := strconv.ParseFloat(s.PhysicsConfig.MoleculeMass, 64)
 	if err != nil {
 		parse(err)
 	}
-	muMol, err := strconv.ParseFloat(pc.MoleculeMass, 64)
-	if err != nil {
-		parse(err)
-	}
-	muAtom, err := strconv.ParseFloat(pc.AtomMass, 64)
+	muAtom, err := strconv.ParseFloat(s.PhysicsConfig.AtomMass, 64)
 	if err != nil {
 		parse(err)
 	}
 
-	var C = make([]float64, bc)
-	for j := 0; j < bc; j += 1 {
-		C[j] = (muMol * muAtom) / (4 * math.Pi * e0 * math.Pow(math.Abs(s.Bath[j].Distance), 3)) *
-			0.5 * (1.0 - 3.0*math.Pow(math.Cos(s.Bath[j].Angle), 2))
+	c := (muMol * muAtom) / (4 * math.Pi * e0 * math.Pow(math.Abs(s.Bath[j-1].Distance), 3)) *
+		0.5 * (1.0 - 3.0*math.Pow(math.Cos(s.Bath[j-1].Angle), 2))
+	return c
+}
+
+func (s *System) hamiltonianHeisenbergTermAt(j int) *mat.Dense {
+	bc, err := strconv.Atoi(s.PhysicsConfig.BathCount)
+	if err != nil {
+		parse(err)
 	}
-	return C
+	spin, err := strconv.ParseFloat(s.PhysicsConfig.Spin, 64)
+	if err != nil {
+		parse(err)
+	}
+
+	dim := bc + 1
+	f := s.forceAt(j)
+	h := manyBody(Sp(spin), 0, dim)
+	h.Mul(h, manyBody(Sm(spin), j, dim))
+	h2 := manyBody(Sm(spin), 0, dim)
+	h2.Mul(h2, manyBody(Sp(spin), j, dim))
+	h.Add(h, h2)
+	h.Apply(func(i, j int, v float64) float64 { return v * f }, h)
+
+	return h
+}
+
+func (s *System) hamiltonianMagneticTerm(b0, b float64) *mat.Dense {
+	bc, err := strconv.Atoi(s.PhysicsConfig.BathCount)
+	if err != nil {
+		parse(err)
+	}
+	spin, err := strconv.ParseFloat(s.PhysicsConfig.Spin, 64)
+	if err != nil {
+		parse(err)
+	}
+
+	h := manyBody(Sz(spin), 0, bc+1)
+	h.Apply(func(i, j int, v float64) float64 { return v * (b0 - b) }, h)
+	return h
+}
+
+func (s *System) hamiltonian(b0, b float64) *mat.Dense {
+	spin, err := strconv.ParseFloat(s.PhysicsConfig.Spin, 64)
+	if err != nil {
+		parse(err)
+	}
+	spin_dim := int(2.0*spin + 1.0)
+	bc, err := strconv.Atoi(s.PhysicsConfig.BathCount)
+	if err != nil {
+		parse(err)
+	}
+	dim := int(math.Pow(float64(spin_dim), float64(bc)+1.0))
+	h := mat.NewDense(dim, dim, nil)
+	for j := 0; j <= bc; j += 1 {
+		h.Add(h, s.hamiltonianHeisenbergTermAt(j))
+	}
+	h.Add(h, s.hamiltonianMagneticTerm(b0, b))
+
+	return h
 }
--- internal/quantum_simulator/system_test.go
+++ internal/quantum_simulator/system_test.go
@@ -2,51 +2,189 @@ package quantum_simulator
 
 import (
 	"math"
+	"reflect"
 	"testing"
+
+	"gonum.org/v1/gonum/mat"
 )
 
-func almostEqual(x []float64, y []float64, eps float64) bool {
-	if len(x) != len(y) {
-		return false
-	}
-	for i := 0; i < len(x); i += 1 {
-		if math.Abs(x[i]-y[i]) > eps {
-			return false
-		}
-	}
-	return true
-}
-
-func TestSystem_forces(t *testing.T) {
+func TestSystem_forceAt(t *testing.T) {
 	type fields struct {
-		CentralSpin State
-		Bath        []State
+		CentralSpin   State
+		Bath          []State
+		PhysicsConfig PhysicsConfig
 	}
 	type args struct {
-		pc PhysicsConfig
+		j int
 	}
 	tests := []struct {
 		name   string
 		fields fields
 		args   args
-		want   []float64
+		want   float64
 	}{
-		// TODO: Add test cases.
+		{
+			name:   "central",
+			fields: fields{CentralSpin: State{0.0, 1.0}, Bath: []State{{0.0, 1.0}}, PhysicsConfig: PhysicsConfig{MoleculeMass: "1.1e-10", AtomMass: "1.0", BathCount: "1"}},
+			args:   args{0},
+			want:   0.0,
+		},
 		{
 			name:   "test",
-			fields: fields{CentralSpin: State{0.0, 1.0, 0.0, 0.0}, Bath: []State{{0.0, 1.0, 0.0, 0.0}}},
-			args:   args{PhysicsConfig{MoleculeMass: "1.1e-10", AtomMass: "1.0", BathCount: "1"}},
-			want:   []float64{-0.98865},
+			fields: fields{CentralSpin: State{0.0, 1.0}, Bath: []State{{0.0, 1.0}}, PhysicsConfig: PhysicsConfig{MoleculeMass: "1.1e-10", AtomMass: "1.0", BathCount: "1"}},
+			args:   args{1},
+			want:   -0.98865,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &System{
-				CentralSpin: tt.fields.CentralSpin,
-				Bath:        tt.fields.Bath,
+				CentralSpin:   tt.fields.CentralSpin,
+				Bath:          tt.fields.Bath,
+				PhysicsConfig: tt.fields.PhysicsConfig,
 			}
-			if got := s.forces(tt.args.pc); !almostEqual(got, tt.want, 1e-4) {
-				t.Errorf("System.forces() = %v, want %v", got, tt.want)
+			if got := s.forceAt(tt.args.j); math.Abs(got-tt.want) > 1e-4 {
+				t.Errorf("System.forceAt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSystem_hamiltonianHeisenbergTermAt(t *testing.T) {
+	type fields struct {
+		CentralSpin   State
+		Bath          []State
+		PhysicsConfig PhysicsConfig
+	}
+	type args struct {
+		j int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *mat.Dense
+	}{
+		{
+			name:   "first",
+			fields: fields{CentralSpin: State{0.0, 1.0}, Bath: []State{{0.0, 1.0}}, PhysicsConfig: PhysicsConfig{MoleculeMass: "1.1e-10", AtomMass: "1.0", BathCount: "1", Spin: "0.5"}},
+			args:   args{1},
+			want: mat.NewDense(4, 4, []float64{
+				0.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, -0.98865, 0.0,
+				0.0, -0.98865, 0.0, 0.0,
+				0.0, 0.0, 0.0, 0.0,
+			}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &System{
+				CentralSpin:   tt.fields.CentralSpin,
+				Bath:          tt.fields.Bath,
+				PhysicsConfig: tt.fields.PhysicsConfig,
+			}
+			if got := s.hamiltonianHeisenbergTermAt(tt.args.j); !mat.EqualApprox(got, tt.want, 1e-4) {
+				t.Errorf("System.hamiltonianHeisenbergTermAt() = %v, want %v", mat.Formatted(got), mat.Formatted(tt.want))
+			}
+		})
+	}
+}
+
+func TestSystem_hamiltonianMagneticTerm(t *testing.T) {
+	type fields struct {
+		CentralSpin   State
+		Bath          []State
+		PhysicsConfig PhysicsConfig
+	}
+	type args struct {
+		b0 float64
+		b  float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *mat.Dense
+	}{
+		{
+			name:   "test",
+			fields: fields{CentralSpin: State{0.0, 1.0}, Bath: []State{{0.0, 1.0}}, PhysicsConfig: PhysicsConfig{MoleculeMass: "1.1e-10", AtomMass: "1.0", BathCount: "1", Spin: "0.5"}},
+			args:   args{b0: 1.0, b: 3.0},
+			want: mat.NewDense(4, 4, []float64{
+				-1.0, 0.0, 0.0, 0.0,
+				0.0, -1.0, 0.0, 0.0,
+				0.0, 0.0, 1.0, 0.0,
+				0.0, 0.0, 0.0, 1.0,
+			}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &System{
+				CentralSpin:   tt.fields.CentralSpin,
+				Bath:          tt.fields.Bath,
+				PhysicsConfig: tt.fields.PhysicsConfig,
+			}
+			if got := s.hamiltonianMagneticTerm(tt.args.b0, tt.args.b); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("System.hamiltonianMagneticTerm() = %v, want %v", mat.Formatted(got), mat.Formatted(tt.want))
+			}
+		})
+	}
+}
+
+func TestSystem_hamiltonian(t *testing.T) {
+	type fields struct {
+		CentralSpin   State
+		Bath          []State
+		PhysicsConfig PhysicsConfig
+	}
+	type args struct {
+		b0 float64
+		b  float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *mat.Dense
+	}{
+		{
+			name:   "test",
+			fields: fields{CentralSpin: State{0.0, 1.0}, Bath: []State{{0.0, 1.0}}, PhysicsConfig: PhysicsConfig{MoleculeMass: "1.1e-10", AtomMass: "1.0", BathCount: "1", Spin: "0.5"}},
+			args:   args{b0: 1.0, b: 3.0},
+			want: mat.NewDense(4, 4, []float64{
+				-1.0, 0.0, 0.0, 0.0,
+				0.0, -1.0, -0.98865, 0.0,
+				0.0, -0.98865, 1.0, 0.0,
+				0.0, 0.0, 0.0, 1.0,
+			}),
+		},
+		{
+			name:   "bigger_system",
+			fields: fields{CentralSpin: State{0.0, 1.0}, Bath: []State{{0.0, 1.0}, {0.0, 2.0}}, PhysicsConfig: PhysicsConfig{MoleculeMass: "1.1e-10", AtomMass: "1.0", BathCount: "2", Spin: "0.5"}},
+			args:   args{b0: 1.0, b: 3.0},
+			want: mat.NewDense(8, 8, []float64{
+				-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, -1.0, 0.0, 0.0, -0.12358, 0.0, 0.0, 0.0,
+				0.0, 0.0, -1.0, 0.0, -0.98865, 0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, -1.0, 0.0, -0.98865, -0.12358, 0.0,
+				0.0, -0.12358, -0.98865, 0.0, 1.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, -0.98865, 0.0, 1.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, -0.1235, 0.0, 0.0, 1.0, 0.0,
+				0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+			}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &System{
+				CentralSpin:   tt.fields.CentralSpin,
+				Bath:          tt.fields.Bath,
+				PhysicsConfig: tt.fields.PhysicsConfig,
+			}
+			if got := s.hamiltonian(tt.args.b0, tt.args.b); !mat.EqualApprox(got, tt.want, 1e-4) {
+				t.Errorf("System.hamiltonian() = \n%v, want \n%v", mat.Formatted(got, mat.Prefix("   "), mat.Squeeze()), mat.Formatted(tt.want))
 			}
 		})
 	}
