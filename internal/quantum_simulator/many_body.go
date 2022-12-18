af1c4714b89252111839ff63793be42020a3f925
Jakub Korsak <jakub.korsak@protonmail.com>
2022-12-18 20:34:32 +0100

    many body works for any casual kind of spin

--- internal/quantum_simulator/many_body.go
+++ internal/quantum_simulator/many_body.go
@@ -6,16 +6,17 @@ import (
 
 // Given an operator from 1-body Hilbert space, return the one-body operator from 'dim'-body Hilbert space, the one-body operator being in slot 'particle' <= 'dim'
 func manyBody(operator *mat.Dense, particle int, dim int) *mat.Dense {
+	spin := float64(operator.RawMatrix().Cols-1) * 0.5
 	if particle > dim {
 		return operator
 	}
 	var m mat.Dense
 	var n mat.Dense
 	if particle > 0 {
-		m = *Id(0.5)
+		m = *Id(spin)
 		for i := 0; i < particle-1; i += 1 {
 			var temp mat.Dense
-			temp.Kronecker(&m, Id(0.5))
+			temp.Kronecker(&m, Id(spin))
 			m = temp
 		}
 		n.Kronecker(&m, operator)
@@ -25,7 +26,7 @@ func manyBody(operator *mat.Dense, particle int, dim int) *mat.Dense {
 	if particle < dim-1 {
 		for i := particle + 1; i < dim; i += 1 {
 			var temp mat.Dense
-			temp.Kronecker(&n, Id(0.5))
+			temp.Kronecker(&n, Id(spin))
 			n = temp
 		}
 	}
