1c0b403842d72e8c41b6977b06db4142bcb00858
Jakub Korsak <jakub@korsak.xyz>
2023-03-21 08:59:36 +0100

    cube

--- internal/quantum_simulator/system.go
+++ internal/quantum_simulator/system.go
@@ -25,7 +25,9 @@ func PolarAngleCos(j int, conf PhysicsConfig) float64 {
 	if conf.Geometry == "ring" {
 		return math.Cos(float64(2*j)*math.Pi/float64(conf.BathCount)) * math.Sin(conf.TiltAngle)
 	} else if conf.Geometry == "cube" && j < 8 {
-		return 0.0 // TODO: write out angles for cube
+		angles := []float64{math.Pi/4.0, math.Pi/4.0, math.Pi/4.0, math.Pi/4.0,
+							3.0*math.Pi/4.0, 3.0*math.Pi/4.0, 3.0*math.Pi/4.0, 3.0*math.Pi/4.0}
+		return angles[j] + conf.TiltAngle
 	}
 	return 0.0
 }
