f01b6351930a3050dbb5072f8036accc4f172a82
Jakub Korsak <jakub@korsak.xyz>
2023-06-05 16:41:10 +0200

    Added tests

--- internal/hilbert_space/state_vector_test.go
+++ internal/hilbert_space/state_vector_test.go
@@ -1,57 +1,60 @@
 package hilbert_space
 
-/*
+import (
+	"math"
+	"testing"
+
+	"gonum.org/v1/gonum/mat"
+)
+
 func TestStateVec_Evolve(t *testing.T) {
 	type args struct {
 		time       float64
-		energies   []complex128
-		eigenBasis []*StateVec
+		energies   []float64
+		eigenBasis *mat.Dense
 	}
 	tests := []struct {
 		name string
-		u    *StateVec
+		u    *mat.VecDense
 		args args
-		want *StateVec
+		want *mat.VecDense
 	}{
 		{
 			name: "Trivial example",
-			u:    NewKet([]complex128{complex(1.0, 0.0), complex(0.0, 0.0)}),
+			u:    mat.NewVecDense(2, []float64{1.0, 0.0}),
 			args: args{
 				time:       0.0,
-				energies:   []complex128{complex(1.0, 0.0), complex(1.0, 0.0)},
-				eigenBasis: []*StateVec{NewKet([]complex128{complex(1.0, 0.0), complex(0.0, 1.0)}), NewKet([]complex128{complex(0.0, 0.0), complex(1.0, 0.0)})},
+				energies:   []float64{1.0, 1.0},
+				eigenBasis: mat.NewDense(2, 2, []float64{1.0, 0.0, 0.0, 1.0}),
 			},
-			want: NewKet([]complex128{complex(1.0, 0.0), complex(0.0, 0.0)}),
+			want: mat.NewVecDense(2, []float64{1.0, 0.0}),
 		},
 		{
 			name: "Large trivial",
-			u:    NewKetReal(ManyBodyVector("uu", 2)),
+			u:    mat.NewVecDense(4, ManyBodyVector("uu", 2)),
 			args: args{
 				time:       0.0,
-				energies:   []complex128{complex(1.0, 0.0), complex(1.0, 0.0), complex(1.0, 0.0), complex(1.0, 0.0)},
-				eigenBasis: []*StateVec{NewKet([]complex128{complex(1.0, 0.0), complex(0.0, 1.0), complex(0.0, 0.0), complex(0.0, 0.0)}), NewKet([]complex128{complex(0.0, 0.0), complex(1.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0)}), NewKet([]complex128{complex(1.0, 0.0), complex(0.0, 1.0), complex(0.0, 0.0), complex(0.0, 0.0)}), NewKet([]complex128{complex(0.0, 0.0), complex(1.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0)})},
+				energies:   []float64{1.0, 1.0, 1.0, 1.0},
+				eigenBasis: mat.NewDense(4, 4, []float64{1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0}),
 			},
-			want: &StateVec{N: 4, Inc: 1, Data: []complex128{
-				complex(2.0, 0.0), complex(0.0, 2.0), complex(0.0, 0.0), complex(0.0, 0.0),
-			}},
+			want: mat.NewVecDense(4, []float64{2.0, 0.0, 0.0, 0.0}),
 		},
 		{
 			name: "another example",
-			u:    NewKet([]complex128{complex(1.0, 0.0), complex(0.0, 0.0)}),
+			u:    mat.NewVecDense(2, []float64{1.0, 0.0}),
 			args: args{
 				time:       1.0,
-				energies:   []complex128{complex(math.Pi, 0.0), complex(math.Pi, 0.0)},
-				eigenBasis: []*StateVec{NewKet([]complex128{complex(1.0, 0.0), complex(0.0, 0.0)}), NewKet([]complex128{complex(0.0, 0.0), complex(1.0, 0.0)})},
+				energies:   []float64{math.Pi, math.Pi},
+				eigenBasis: mat.NewDense(2, 2, []float64{1.0, 0.0, 0.0, 1.0}),
 			},
-			want: NewKet([]complex128{complex(-1.0, 0.0), complex(0.0, 0.0)}),
+			want: mat.NewVecDense(2, []float64{-1.0, 0.0}),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.u.Evolve(tt.args.time, tt.args.energies, tt.args.eigenBasis); cmplx.Abs(got.Dot(tt.want))-math.Pow(got.Norm(), 2) > 1e-6 {
+			if got := Evolve(tt.u, tt.args.time, tt.args.energies, tt.args.eigenBasis); mat.Dot(got, tt.want)-math.Pow(got.Norm(2), 2) > 1e-8 {
 				t.Errorf("StateVec.At() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
-*/
