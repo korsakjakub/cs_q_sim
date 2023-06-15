3f93b499c648280f762448d4efe9aef7fe3698d0
Jakub Korsak <jakub.korsak@protonmail.com>
2023-06-15 21:59:28 +0200

    Spread couplings vs tilt angle

---
+++ scripts/plot_spread.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import sys
+import yaml
+import numpy as np
+
+def plot(outdir, figdir, filenames):
+    for filename in filenames:
+        if "outputs" in filename:
+            outdir = filename.split("/")[0]
+            filename = filename.split("/")[1]
+        with open(f"{outdir}/{filename}") as res_file:
+            contents = yaml.safe_load(res_file)
+            metadata = contents["metadata"]
+            for xys in contents["xys"]:
+                xs = []
+                ys = []
+                for xy in xys:
+                    xs.append(xy["x"])
+                    ys.append(xy["y"])
+            plt.plot(xs, ys, label=f"Geometry: {contents['values']['system']['physicsconfig']['geometry']}\nN: {contents['values']['system']['physicsconfig']['bathcount']}")
+        p = filename.split("/")[-1]
+    plt.title(metadata["simulation"] + "\n" + r"$A(\beta) = \underset{k}{\max}\,|C_k|-\underset{k}{\min}\,|C_k|$")
+    plt.xlabel(r"$\beta / \pi$")
+    plt.ylabel(r"$2\pi \times \mathrm{kHz}$")
+    plt.legend()
+    plt.savefig(f"{figdir}/spread-couplings-plt-{p}.png")
+
+if __name__ == '__main__':
+    paths = sys.argv[1:]
+    plot("outputs", "figures", paths)
